da1ca02b67e6eed67118eeaa4c453036978d016a
Verigence-admin <jbrconsulting.it@gmail.com>
2026-08-17 17:58:46 +0530

    fix: remove business scope build variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,9 +2,6 @@
 
 interface ImportMetaEnv {
   readonly VITE_AUDIT_CORE_BASE_URL?: string;
-  readonly VITE_TENANT_ID?: string;
-  readonly VITE_DEFAULT_DEALER_ID?: string;
-  readonly VITE_DEFAULT_OUTLET_ID?: string;
 }
 
 interface ImportMeta {

8d69317dfd5a5fae9504a7400d54cece73d30b1d
Verigence-admin <jbrconsulting.it@gmail.com>
2026-08-22 11:22:14 +0530

    config(mobile): define native WebView defaults

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,16 @@ const config: CapacitorConfig = {
   appId: 'com.verigence.app',
   appName: 'Verigence',
   webDir: 'dist',
+  backgroundColor: '#011e47',
+  loggingBehavior: 'debug',
+  server: {
+    hostname: 'localhost',
+    iosScheme: 'capacitor',
+    androidScheme: 'https',
+  },
+  ios: {
+    preferredContentMode: 'mobile',
+  },
 };
 
 export default config;

ba807c6b0dc6e1f55c37046e9aa04445b5529d78
Verigence-admin <jbrconsulting.it@gmail.com>
2026-08-23 22:47:07 +0530

    chore(uc02): declare Google Maps embed build variable

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_AUDIT_CORE_BASE_URL?: string;
   readonly VITE_SECURITY_BASE_URL?: string;
+  readonly VITE_GOOGLE_MAPS_EMBED_API_KEY?: string;
 }
 
 interface ImportMeta {
